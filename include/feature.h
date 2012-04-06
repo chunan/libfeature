99c2b2ac40709a07c23a55c5984f9bface45b3c0
ugoc <ugocugoc@gmail.com>
2012-04-06 22:05:49 +0800

    Change double to float.

--- include/feature.h
+++ include/feature.h
@@ -1,6 +1,7 @@
 #ifndef _FEATURE_H_
 #define _FEATURE_H_
 
+#include <string>
 #include "ugoc_utility.h"
 
 using namespace std;
@@ -11,32 +12,38 @@ class Feature {
   Feature() { Init(); }
   Feature(FILE *fd, char type);
   Feature(string filename);
-  Feature(double *wav, int len);
-  string getFname() { return fname; }
+  Feature(float *wav, int len);
+  string getFname() { return fname_; }
   void DumpData() const;
-  bool LoadFile(string filename);
+  bool LoadFile(const string& filename);
   bool WriteToBin(FILE *fo_bin);
   bool WriteToAscii(FILE *fo_ascii);
   bool ResetFeature();
   ~Feature() {
     ResetFeature();
   }
-  // Data
-  string fname;
-  int LT;
-  int LF;
-  double dT;
-  double dF;
-  double **data;
-  double window;
-  int LT_size;
-  int LF_size;
-  int data_size;
+  /* accessors */
+  int LT() const { return data_.R(); }
+  int LF() const { return data_.C(); }
+  float dT() const { return dT_; }
+  float dF() const { return dF_; }
+  const TwoDimArray<float>& Data() const { return data_; }
+  const float operator()(int t, int f) const {
+    return data_.Entry(t, f);
+  }
+  const float* operator[](int t) const { return data_.Vec(t); }
+
  private:
   bool LoadFromBin(FILE *fi_bin);
   bool LoadFromHTK(FILE *fi_htk);
   bool LoadFromAscii(FILE *fi_ascii);
   void Init();
+  // Data
+  string fname_;
+  float dT_;
+  float dF_;
+  float window_;
+  TwoDimArray<float> data_;
 };
 
 
--- src/feature.cpp
+++ src/feature.cpp
@@ -6,12 +6,15 @@
 
 using namespace std;
 
+const int sizeInt = sizeof(int);
+const int sizeFloat = sizeof(float);
+
 // Feature constructor
 Feature::Feature(FILE *fd, char type) {
   Init();
-  if(type == 'a') {
+  if (type == 'a') {
     LoadFromAscii(fd);
-  } else if(type == 'b') {
+  } else if (type == 'b') {
     LoadFromBin(fd);
   }
 }
@@ -22,168 +25,185 @@ Feature::Feature(string filename) {
 }
 
 void Feature::Init() {
-  LT = 0;
-  LF = 0;
-  dT = 0.0;
-  dF = 0.0;
-  data = NULL;
-  LT_size = 0;
-  LF_size = 0;
-  data_size = 0;
+  fname_.clear();
+  dT_ = 0.0;
+  dF_ = 0.0;
+  window_ = 0.0;
 }
 
 bool Feature::ResetFeature() {
-  mem_op<double>::delete_2d_array(data);
   Init();
   return true;
 }
 
-bool Feature::LoadFile(string filename) {
+bool Feature::LoadFile(const string& filename) {
   /* Check for extension of the file */
   size_t found = filename.find_last_of(".");
-  string ext = filename.substr(found+1);
+  string ext = filename.substr(found + 1);
 
-  FILE *fd = FOPEN(filename.c_str(),"r");
-  if ( ext == "mfc" || ext == "fbank" ){
+  FILE *fd = FOPEN(filename.c_str(), "r");
+  if (ext == "mfc" || ext == "fbank" || ext == "plp") {
     LoadFromHTK(fd);
   }
-  else if ( ext == "spec" ){
+  else if (ext == "spec") {
     LoadFromBin(fd);
   }
   else{
     ErrorExit(__FILE__, __LINE__, 1,"Unknown extension `%s'\n", ext.c_str());
   }
   fclose(fd);
-  fname = filename;
+  fname_ = filename;
   return true;
-
 }
 
 
 // Feature I/O
 bool Feature::WriteToBin(FILE *fo_bin) {
-  assert( data != NULL);
-  static int sizeInt = sizeof(int);
-  static int sizeDouble = sizeof(double);
-  assert( fwrite(&LT, sizeInt, 1, fo_bin) == 1 );
-  assert( fwrite(&LF, sizeInt, 1, fo_bin) == 1 );
-  assert( fwrite(&dT, sizeDouble, 1, fo_bin) == 1 );
-  assert( fwrite(&dF, sizeDouble, 1, fo_bin) == 1 );
-  assert( fwrite(&window, sizeDouble, 1, fo_bin) == 1 );
-  assert( fwrite(data[0], sizeDouble, LT*LF, fo_bin) == unsigned(LT*LF) );
+  assert(data_[0] != NULL);
+  size_t s_write;
+  int ival;
+
+  ival = LT();
+  s_write = fwrite(&ival, sizeInt, 1, fo_bin);
+  assert(s_write == 1);
+  ival = LF();
+  s_write = fwrite(&ival, sizeInt, 1, fo_bin);
+  assert(s_write == 1);
+  s_write = fwrite(&dT_, sizeFloat, 1, fo_bin);
+  assert(s_write == 1);
+  s_write = fwrite(&dF_, sizeFloat, 1, fo_bin);
+  assert(s_write == 1);
+  s_write = fwrite(&window_, sizeFloat, 1, fo_bin);
+  assert(s_write == 1);
+  s_write = fwrite(data_[0], sizeFloat, LT() * LF(), fo_bin);
+  assert(static_cast<int>(s_write) == LT() * LF());
   return true;
 }
 
 bool Feature::LoadFromBin(FILE *fi_bin) {
-  static int sizeInt = sizeof(int);
-  static int sizeDouble = sizeof(double);
-  assert( fread(&LT, sizeInt, 1, fi_bin) == 1 );
-  assert( fread(&LF, sizeInt, 1, fi_bin) == 1 );
-  assert( fread(&dT, sizeDouble, 1, fi_bin) == 1 );
-  assert( fread(&dF, sizeDouble, 1, fi_bin) == 1 );
-  assert( fread(&window, sizeDouble, 1, fi_bin) == 1 );
-  mem_op<double>::reallocate_2d_array(data, LT, LF, LT_size, LF_size, data_size);
-  assert( fread(data[0], sizeDouble, LT*LF, fi_bin) == unsigned(LT*LF) );
+  size_t s_read;
+  int len_t, len_f;
+  s_read = fread(&len_t, sizeInt, 1, fi_bin);
+  assert(s_read == 1);
+  s_read = fread(&len_f, sizeInt, 1, fi_bin);
+  assert(s_read == 1);
+  s_read = fread(&dT_, sizeFloat, 1, fi_bin);
+  assert(s_read == 1);
+  s_read = fread(&dF_, sizeFloat, 1, fi_bin);
+  assert(s_read == 1);
+  s_read = fread(&window_, sizeFloat, 1, fi_bin);
+  assert(s_read == 1);
+  data_.Resize(len_t, len_f);
+  s_read = fread(data_[0], sizeFloat, LT() * LF(), fi_bin);
+  assert(static_cast<int>(s_read) == LT() * LF());
   return true;
 }
 
 bool Feature::WriteToAscii(FILE *fo_ascii) {
-  assert( data != NULL);
+  assert(data_[0] != NULL);
   bool ret = true;
-  if(fprintf(fo_ascii, "LT=%d, LF=%d, dT=%f, dF=%f, window=%f\n", LT, LF, dT, dF, window) < 0) ret = false;
-  for(int t=0; t<LT; t++)
-  {
-    if(fprintf(fo_ascii, "%.2f", data[t][0]) < 0) ret = false;
-    for(int f=1;f<LF;f++) if(fprintf(fo_ascii, "\t%.2f", data[t][f]) < 0) ret = false;
-    if(fprintf(fo_ascii, "\n") < 0) ret = false;
+  if (fprintf(fo_ascii, "LT=%d, LF=%d, dT=%f, dF=%f, window=%f\n",
+              LT(), LF(), dT_, dF_, window_) < 0) ret = false;
+  for (int t = 0; t < LT(); ++t) {
+    if (fprintf(fo_ascii, "%.2f", data_(t, 0)) < 0)
+      ret = false;
+    for (int f = 1; f < LF(); ++f) {
+      if (fprintf(fo_ascii, "\t%.2f", data_(t, f)) < 0) ret = false;
+    }
+    if (fprintf(fo_ascii, "\n") < 0) ret = false;
   }
   return ret;
 }
 
 bool Feature::LoadFromAscii(FILE *fi_ascii) {
   bool ret = true;
-  if(fscanf(fi_ascii, "LT=%d, LF=%d, dT=%lf, dF=%lf, window=%lf\n",
-            &LT, &LF, &dT, &dF, &window) < 0)
+  int len_t, len_f;
+  if (fscanf(fi_ascii, "LT=%d, LF=%d, dT=%f, dF=%f, window=%f\n",
+             &len_t, &len_f, &dT_, &dF_, &window_) < 0) {
     ret = false;
+  }
 
-  mem_op<double>::reallocate_2d_array(data, LT, LF, LT_size, LF_size, data_size);
-  for(int t=0; t<LT; t++)
-  {
-    if(fscanf(fi_ascii, "%lf", &data[t][0]) != 1) ret = false;
-    for(int f=1;f<LF;f++) if(fscanf(fi_ascii, "\t%lf", &data[t][f]) != 1) ret = false;
-    if(fscanf(fi_ascii, "\n" ) != 0) ret = false;
+  data_.Resize(len_t, len_f);
+  for (int t = 0; t < LT(); ++t) {
+    if (fscanf(fi_ascii, "%f", &data_[t][0]) != 1)
+      ret = false;
+    for (int f = 1; f < LF(); ++f) {
+      if (fscanf(fi_ascii, "\t%f", &data_[t][f]) != 1) {
+        ret = false;
+      }
+    }
+    if (fscanf(fi_ascii, "\n") != 0) ret = false;
   }
   return ret;
 }
 
 bool Feature::LoadFromHTK(FILE *fi_htk) {
-  static int sizeInt = sizeof(int);
-  static int sizeDouble = sizeof(double);
 
   int sampPeriod = 0, sampSize = 0, parmKind = 0;
+  int len_t, len_f;
+  size_t s_read;
 
-  assert( fread(&LT, 4, 1, fi_htk) == 1 );
-  assert( fread(&sampPeriod, 4, 1, fi_htk) == 1 );
-  assert( fread(&sampSize, 2, 1, fi_htk) == 1 );
-  assert( fread(&parmKind, 2, 1, fi_htk) == 1 );
+  s_read = fread(&len_t, 4, 1, fi_htk);
+  assert(s_read == 1);
+  s_read = fread(&sampPeriod, 4, 1, fi_htk);
+  assert(s_read == 1);
+  s_read = fread(&sampSize, 2, 1, fi_htk);
+  assert(s_read == 1);
+  s_read = fread(&parmKind, 2, 1, fi_htk);
+  assert(s_read == 1);
 
-  if((02000 & parmKind) > 0){
-    LF = sampSize/2;
+  if ((02000 & parmKind) > 0) {
+    len_f = sampSize / 2;
     sampSize = 2;
-  }else{
-    LF = sampSize/4;
+  } else {
+    len_f = sampSize / 4;
     sampSize = 4;
   }
 
-  dT = double(sampPeriod) * 1e-7;
-  dF = window = 0.0;
+  dT_ = static_cast<float>(sampPeriod) * 1e-7;
+  dF_ = window_ = 0.0;
 
-  mem_op<double>::reallocate_2d_array(data, LT, LF, LT_size, LF_size, data_size);
+  data_.Resize(len_t, len_f);
   /* Uncompressed type */
-  if(sampSize == 4){
-    if(sizeDouble == sampSize) {
-      assert( fread(data[0], sampSize, LT*LF, fi_htk) == unsigned(LT*LF) );
+  if (sampSize == 4) {
+    if (sizeFloat == sampSize) {
+      s_read = fread(data_[0], sampSize, len_t * len_f, fi_htk);
+      assert(static_cast<int>(s_read) == len_t * len_f);
     } else {
-      unsigned totnum = LT * LF;
+      unsigned totnum = len_t * len_f;
       float *ptr = new float [totnum];
-      assert( fread(ptr, sampSize, totnum, fi_htk) == totnum );
-      for(unsigned int i = 0; i < totnum; i++)
-        data[0][i] = static_cast<double>(ptr[i]);
+      assert(fread(ptr, sampSize, totnum, fi_htk) == totnum);
+      for (unsigned int i = 0; i < totnum; i++) {
+        data_(0, i) = static_cast<float>(ptr[i]);
+      }
       delete [] ptr;
     }
   }
   /* Compressed type */
-  else if(sampSize == 2){
-    if(sizeInt == sampSize){
-      int **ptr = mem_op<int>::new_2d_array(LT, LF);
-      assert( fread(data[0], sizeInt, LT*LF, fi_htk) == unsigned(LT*LF) );
-      for(int t = 0; t < LT; t++){
-        for(int d = 0; d < LF; d++)
-          data[t][d] = static_cast<double>(ptr[t][d]);
+  else if (sampSize == 2) {
+    if (sizeInt == sampSize) {
+      int **ptr = mem_op<int>::new_2d_array(len_t, len_f);
+      s_read = fread(data_[0], sizeInt, len_t*len_f, fi_htk);
+      assert(static_cast<int>(s_read) == len_t * len_f);
+      for (int t = 0; t < len_t; t++) {
+        for (int f = 0; f < len_f; f++) {
+          data_(t, f) = static_cast<float>(ptr[t][f]);
+        }
       }
-      mem_op<int>::delete_2d_array(ptr);
+      mem_op<int>::delete_2d_array(&ptr);
     }
     else {
       // Not implement yet.
       ErrorExit(__FILE__, __LINE__, 1,"Int size =/= 2, not implement yet\n");
     }
   }
-  //cout << "LF = " << LF << ", LT = " << LT << endl;
   return true;
 }
 
 void Feature::DumpData() const {
   printf("DATA:\n");
-  printf("(LT, LF) = (%d, %d)\n", LT, LF);
-  printf("(dT, dF) = (%gs, %gHz)\n", dT, dF);
-  printf("window = %gs\n", window);
+  printf("(dT, dF) = (%gs, %gHz)\n", dT_, dF_);
+  printf("window = %gs\n", window_);
   printf("PRIVATE:\n");
-  printf("(LT_size, LF_size, data_size) = (%d, %d, %d)\n", LT_size, LF_size, data_size);
-  for (int t = 0; t < LT; t++){
-    printf("%d: ", t);
-    for (int f = 0; f < LF; f++){
-      printf("%.2f ", data[t][f]);
-    }
-    printf("\n");
-  }
+  data_.DumpData();
 }
